a2a34e3f8bd21e4f2c8bdab753bbaa42c257b351
ophanim777 <rikyjo49@gmail.com>
2026-06-21 23:13:24 +0700

    add seed faker

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -1,17 +1,52 @@
+const bcrypt = require("bcryptjs");
+const { faker } = require("@faker-js/faker");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 async function main() {
 
+  const password =
+    await bcrypt.hash("123456",10);
+
+  for(let i=0;i<20;i++){
+
+    await prisma.user.create({
+
+      data:{
+
+        firstName:
+        faker.person.firstName(),
+
+        lastName:
+        faker.person.lastName(),
+
+        email:
+        faker.internet.email(),
+
+        password,
+
+        bio:
+        faker.person.bio()
+
+      }
+
+    });
+
+  }
+
 }
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async err => {
-    console.log(err);
+.then(async()=>{
 
-    await prisma.$disconnect();
-  });
+await prisma.$disconnect();
+
+})
+.catch(async(err)=>{
+
+console.log(err);
+
+await prisma.$disconnect();
+
+});
