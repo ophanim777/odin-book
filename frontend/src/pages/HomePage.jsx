34462507a52a9e523e80a0bc183a2004c1dd6ac1
ophanim777 <rikyjo49@gmail.com>
2026-06-21 23:34:54 +0700

    add homepage structure

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar";
+
+function HomePage() {
+
+  return (
+    <>
+      <Navbar />
+
+      <div className="container mt-4">
+
+        <h2>Timeline</h2>
+
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
