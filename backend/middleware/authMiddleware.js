b678d7ee5ba5f65c2e5437df09d0312178eee61c
ophanim777 <rikyjo49@gmail.com>
2026-06-21 22:28:29 +0700

    add authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,15 @@
+exports.isAuthenticated = (
+  req,
+  res,
+  next
+) => {
+
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  return res.status(401).json({
+    message: "Unauthorized"
+  });
+
+};
