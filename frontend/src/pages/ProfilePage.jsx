1d917383b341b79b4d9ee7a88c50b7511e255d3e
ophanim777 <rikyjo49@gmail.com>
2026-06-21 23:36:42 +0700

    add profile page structure

---
+++ frontend/src/pages/ProfilePage.jsx
@@ -0,0 +1,21 @@
+function ProfilePage() {
+
+  return (
+    <div className="container mt-5">
+
+      <h1>Profile</h1>
+
+      <img
+        width="120"
+        className="rounded-circle"
+      />
+
+      <h3>User Name</h3>
+
+      <p>User Bio</p>
+
+    </div>
+  );
+}
+
+export default ProfilePage;
