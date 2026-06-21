4a6b32dfdf8e6527a565cf87450541d9d669e66f
ophanim777 <rikyjo49@gmail.com>
2026-06-21 23:35:54 +0700

    add create post page structure

---
+++ frontend/src/pages/CreatePostPage.jsx
@@ -0,0 +1,20 @@
+function CreatePostPage() {
+
+  return (
+    <div className="container mt-5">
+
+      <h2>Create Post</h2>
+
+      <textarea
+        className="form-control mb-3"
+      />
+
+      <button className="btn btn-primary">
+        Post
+      </button>
+
+    </div>
+  );
+}
+
+export default CreatePostPage;
