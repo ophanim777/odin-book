724f156d77fe1a042e668919a426e0db98d5ffee
ophanim777 <rikyjo49@gmail.com>
2026-06-21 22:33:10 +0700

    add authentication controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,60 @@
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
+
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+exports.register = async (req, res) => {
+
+  try {
+
+    const {
+      firstName,
+      lastName,
+      email,
+      password
+    } = req.body;
+
+    const existingUser =
+      await prisma.user.findUnique({
+        where: {
+          email
+        }
+      });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "Email already exists"
+      });
+    }
+
+    const hashedPassword =
+      await bcrypt.hash(password, 10);
+
+    const user =
+      await prisma.user.create({
+        data: {
+          firstName,
+          lastName,
+          email,
+          password: hashedPassword
+        }
+      });
+
+    res.status(201).json({
+      message: "User created",
+      user
+    });
+
+  } catch (err) {
+
+    console.log(err);
+
+    res.status(500).json({
+      message: "Server error"
+    });
+
+  }
+
+};
